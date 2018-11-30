deda2f7d6ac93c98b09fb43364c50a2257df25c8
Adeniran Adeniyi <aaden001@odu.edu>
2018-11-30 17:55:38 -0500

    Git hub sign-in integration

--- index.php
+++ index.php
@@ -1,3 +1,89 @@
+<?php  include('services.php');
+  session_start();
+/*parse_str(implode('&', array_slice($argv, 1)), $_GET);*/
+
+  if( isset($_SESSION['authenticationFlag']) === true )
+  {
+    header('Location: main.php?channel=general');
+    exit;
+  }
+
+  gitLogin();
+
+  //credit: https://gist.github.com/asika32764/b204ff4799d577fd4eef
+  function gitLogin()
+  {
+    define('OAUTH2_CLIENT_ID', '541b6e510ce03d138842');
+    define('OAUTH2_CLIENT_SECRET', '728df75aa33cd44226a78ee687be6df49f69a722');
+
+    $authorizeURL = 'https://github.com/login/oauth/authorize';
+    $tokenURL     = 'https://github.com/login/oauth/access_token';
+    $apiURLBase   = 'https://api.github.com/';
+
+
+    // Start the login process by sending the user to Github's authorization page
+    if(get('action') == 'login') 
+    {
+        // Generate a random hash and store in the session for security
+        $_SESSION['state'] = hash('sha256', microtime(TRUE) . rand() . $_SERVER['REMOTE_ADDR']);
+        unset($_SESSION['access_token']);
+        $params = array(
+            'client_id' => OAUTH2_CLIENT_ID,
+            'redirect_uri' => 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['PHP_SELF'],
+            'scope' => 'user',
+            'state' => $_SESSION['state']
+        );
+        // Redirect the user to Github's authorization page
+        header('Location: ' . $authorizeURL . '?' . http_build_query($params));
+        die();
+    }
+
+    // When Github redirects the user back here, there will be a "code" and "state" parameter in the query string
+    if (get('code')) 
+    {
+        // Verify the state matches our stored state
+        if (!get('state') || $_SESSION['state'] != get('state')) {
+            header('Location: ' . $_SERVER['PHP_SELF']);
+            die();
+        }
+        // Exchange the auth code for a token
+      $token = apiRequest($tokenURL, array(
+      'client_id' => OAUTH2_CLIENT_ID,
+      'client_secret' => OAUTH2_CLIENT_SECRET,
+      'redirect_uri' => 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['PHP_SELF'],
+      'state' => $_SESSION['state'],
+      'code' => get('code')
+      ));
+      $_SESSION['access_token'] = $token->access_token;
+      header('Location: ' . $_SERVER['PHP_SELF']);
+    }
+  }
+
+
+  function apiRequest($url, $post = FALSE, $headers = array())
+  {
+      $ch = curl_init($url);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+      if ($post)
+          curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
+      $headers[] = 'Accept: application/json';
+      if (session('access_token'))
+          $headers[] = 'Authorization: Bearer ' . session('access_token');
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+      $response = curl_exec($ch);
+      return json_decode($response);
+  }
+
+  function get($key, $default = NULL)
+  {
+      return array_key_exists($key, $_GET) ? $_GET[$key] : $default;
+  }
+
+  function session($key, $default = NULL)
+  {
+      return array_key_exists($key, $_SESSION) ? $_SESSION[$key] : $default;
+  }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -29,7 +115,18 @@
  -->  
     <div class="g-recaptcha" data-sitekey="6LcuJHkUAAAAAK_9o842eo2aN0aYvTmbYdavnxvB" ></div>
     <button type="submit" class="btn btn-primary">Sign in</button>
-      
+      <?php
+            echo "<br>";
+          if (session('access_token')) 
+          {
+              echo '<h3>Git: Logged In</h3>';
+          } 
+          else 
+          {
+              echo '<h3>Git: Not logged in</h3>';
+              echo '<p><a href="?action=login">Log In</a></p>';
+          }
+        ?>
       <b><a href="index2.php">Click to Sign Up</a></b>
       
         
