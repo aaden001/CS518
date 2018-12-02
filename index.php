385217df0b6bd5adeb51edc87feeffe46fe6e1e5
Adeniran Adeniyi <aaden001@odu.edu>
2018-12-01 22:32:10 -0500

    real Checking git hub data6

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ error_reporting(E_ALL);
         $params = array(
             'client_id' => OAUTH2_CLIENT_ID,
             'redirect_uri' => 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['PHP_SELF'],
-            'scope' => 'user',
+            'scope' => '',
             'state' => $_SESSION['state']
         );
         // Redirect the user to Github's authorization page
