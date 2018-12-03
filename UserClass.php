925c01ec04a76159e5ee1be103d7f581890eb19e
Adeniran Adeniyi <aaden001@odu.edu>
2018-12-02 22:46:17 -0500

    debugging loging 3

--- UserClass.php
+++ UserClass.php
@@ -77,30 +77,55 @@ class User{
 	public function SignUpUser(){
 
 			try{
-			include "dbconnect.php";
-			$Sql = $Connection->prepare("INSERT INTO Users(userFullName,userEmail,userHandle,userPassword) VALUES (:tempFullName,:tempUserMail,:tempHandle,:tempPasswd)");
-			$Sql->execute(array('tempFullName' => $this->getUserFullname(),'tempUserMail' => $this->getUserEmail(), 'tempHandle' => $this->getUserHandle(),'tempPasswd' => $this->getUserPassword()));
+				if(isset($_SESSION['access_token'])){
+					include "dbconnect.php";
+					$Sql = $Connection->prepare("INSERT INTO Users(userFullName,userEmail,userHandle,userPassword) VALUES (:tempFullName,:tempUserMail,:tempHandle,:tempPasswd)");
+					$Sql->execute(array('tempFullName' => $this->getUserFullname(),'tempUserMail' => $this->getUserEmail(), 'tempHandle' => $this->getUserHandle(),'tempPasswd' => $this->getUserPassword()));
 
-			///Add user to the global chat room instantly
-			/* By checking a matching user name and password a getting the Id 
-			Use the Id to insert the user into the global chat room
-			*/
-			$sql = "SELECT * FROM Users WHERE userEmail=:userMail AND userPassword=:password";
-			/// Execute a prepared statement with an array of insert values (named parameters)
-			$query = $Connection->prepare($sql);
-			$query->execute(array('userMail' =>$this->getUserEmail(),'password' => $this->getUserPassword()));
-			$result = $query->fetch();
-			$this->setUserId($result['userId']);
-			$room = 1;
-			$this->AddUserToRoom($this->getUserId(),$room);
+					///Add user to the global chat room instantly
+					/* By checking a matching user name and password a getting the Id 
+					Use the Id to insert the user into the global chat room
+					*/
+					$sql = "SELECT * FROM Users WHERE userEmail=:userMail AND userPassword=:password";
+					/// Execute a prepared statement with an array of insert values (named parameters)
+					$query = $Connection->prepare($sql);
+					$query->execute(array('userMail' =>$this->getUserEmail(),'password' => $this->getUserPassword()));
+					$result = $query->fetch();
+					$this->setUserId($result['userId']);
+					$room = 1;
+					$this->AddUserToRoom($this->getUserId(),$room);
 
-			/*$queryG = $Connection->prepare("INSERT INTO UserGroups(UserID,RoomsID) VALUES (:tempUserID,:temGlobalRoom)");
-			$queryG->execute(array('tempUserID' => $this->getUserId(), 'temGlobalRoom' => $room));*/
+					/*$queryG = $Connection->prepare("INSERT INTO UserGroups(UserID,RoomsID) VALUES (:tempUserID,:temGlobalRoom)");
+					$queryG->execute(array('tempUserID' => $this->getUserId(), 'temGlobalRoom' => $room));*/
 
-			$Connection = null;
-			/*
-			echo "Sign Up Successfull";
-			header("Refresh:5; url=index.php",true, 303);*/
+					$Connection = null;
+				}else{
+					include "dbconnect.php";
+					$Sql = $Connection->prepare("INSERT INTO Users(userFullName,userEmail,userHandle,userPassword) VALUES (:tempFullName,:tempUserMail,:tempHandle,:tempPasswd)");
+					$Sql->execute(array('tempFullName' => $this->getUserFullname(),'tempUserMail' => $this->getUserEmail(), 'tempHandle' => $this->getUserHandle(),'tempPasswd' => $this->getUserPassword()));
+
+					///Add user to the global chat room instantly
+					/* By checking a matching user name and password a getting the Id 
+					Use the Id to insert the user into the global chat room
+					*/
+					$sql = "SELECT * FROM Users WHERE userEmail=:userMail AND userPassword=:password";
+					/// Execute a prepared statement with an array of insert values (named parameters)
+					$query = $Connection->prepare($sql);
+					$query->execute(array('userMail' =>$this->getUserEmail(),'password' => $this->getUserPassword()));
+					$result = $query->fetch();
+					$this->setUserId($result['userId']);
+					$room = 1;
+					$this->AddUserToRoom($this->getUserId(),$room);
+
+					/*$queryG = $Connection->prepare("INSERT INTO UserGroups(UserID,RoomsID) VALUES (:tempUserID,:temGlobalRoom)");
+					$queryG->execute(array('tempUserID' => $this->getUserId(), 'temGlobalRoom' => $room));*/
+
+					$Connection = null;
+					
+					echo "Sign Up Successfull";
+					header("Refresh:5; url=index.php",true, 303);
+				}
+			
 			}catch(PDOException $e){
 			echo $e->getMessage();
 			}
@@ -168,8 +193,8 @@ class User{
 
 			if(isset($_SESSION['access_token'])){
 
-				
-				
+
+
 			}else{
 					if($query->rowCount() == 0)
 					{
