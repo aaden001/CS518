1e046dc4eefacd6bf11ae3b73bd83035622808d2
Adeniran Adeniyi <aaden001@odu.edu>
2018-12-03 20:09:44 -0500

    2FA working on it file path

--- 2Fa.php
+++ 2Fa.php
@@ -1,7 +1,10 @@
 <?php 
-	require 'vendor/autoload.php';
-	$authenticator = new PHPGangsta_GoogleAuthenticator();
-	$secret = $authenticator->createSecret();
-	echo "Secret: ".$secret;
- 
+
+declare(strict_types=1);
+include_once __DIR__.'src/FixedBitNotation.php';
+include_once __DIR__.'src/GoogleAuthenticator.php';
+ $g = new \Google\Authenticator\GoogleAuthenticator();
+$salt = '7WAO342QFANY6IKBF7L7SWEUU79WL3VMT920VB5NQMW';
+$secret = $username.$salt;
+echo '<img src="'.$g->getURL($username, 'http://aaden001.cs518.cs.odu.edu', $secret).'" />';
 ?>
