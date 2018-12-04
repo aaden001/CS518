e6b314d3b7becb1c9b7ec33f2a904cbf30bf034a
Adeniran Adeniyi <aaden001@odu.edu>
2018-12-04 12:35:39 -0500

    All in all

--- 2Fa.php
+++ 2Fa.php
@@ -7,14 +7,21 @@
     session_start();
 
 if(isset($_SESSION['userEmail']))
-  {
-		
-		/*include_once 'src/FixedBitNotation.php';*/
-		include_once 'src/GoogleAuthenticator.php';
-		$g = new \Google\Authenticator\GoogleAuthenticator();
+  {	
+
+		include_once __DIR__.'/sonata/FixedBitNotation.php';
+		include_once __DIR__.'/sonata/GoogleAuthenticator.php';
+  	 $username = $_SESSION['userName'];
+		/*$g = new \Google\Authenticator\GoogleAuthenticator();*/
+		$g = new GoogleAuthenticator();
 		$salt = '7WAO342QFANY6IKBF7L7SWEUU79WL3VMT920VB5NQMW';
-		$secret = $_SESSION['userEmail'].$salt;
-		echo '<img src="'.$g->getURL($_SESSION['userEmail'], 'http://aaden001.cs518.cs.odu.edu', $secret).'" />';
+		$secret = $username.$salt;
+		echo '<img src="'.$g->getURL("", 'http://192.168.56.152/', $secret).'" />';
+
+		$g = new GoogleAuthenticator();
+		$salt = '7WAO342QFANY6IKBF7L7SWEUU79WL3VMT920VB5NQMW';
+		$secret = $username.$salt;
+
 		echo '<form class="form-inline" action="" method="post">
 		<div class="form-group">
 		<label for="inputPassword6">Code Input</label>
@@ -23,20 +30,85 @@ if(isset($_SESSION['userEmail']))
 
 		</div>
 		</form>';
+
+
+		if( isset($_POST['SubmitButton']) && isset($_POST['inputTest'])){
+			$userInput = $_POST['inputTest'];
+
+			$checkResult = $g->checkCode($secret, $userInput);    // 2 = 2*30sec clock tolerance
+			if ($checkResult) {
+			echo 'OK';
+			} else {
+			echo 'FAILED';
+			}	
+		}
+
+		//$check_this_code =$g->getCode($secret);
+/*
+			
+		$ga = new PHPGangsta_GoogleAuthenticator();
+		$secret = $ga->createSecret();
+		
+
+		$qrCodeUrl = $ga->getQRCodeGoogleUrl('http://192.168.56.152/', $secret);
+
+
+		echo '<img src="'.$qrCodeUrl.'" /><br><br>';
+
+		echo '<form class="form-inline" action="" method="post">
+		<div class="form-group">
+		<label for="inputPassword6">Code Input</label>
+		<input type="text" name="inputTest" id="inputPassword" class="form-control" />
+		<input type="submit" name="SubmitButton"/>
+
+		</div>
+		</form>';
+
+		///$oneCode = $ga->getCode($secret);
+		echo "Checking Code '$oneCode' and Secret '$secret':\n";
+		if( isset($_POST['SubmitButton']) && isset($_POST['inputTest'])){
+			$userInput = $_POST['inputTest'];
+
+			$checkResult = $ga->verifyCode($secret, $userInput, 10);    // 2 = 2*30sec clock tolerance
+			if ($checkResult) {
+			echo 'OK';
+			} else {
+			echo 'FAILED';
+			}	
+		}
+		*/
+
+
+		
+/*		echo '<img src="'.$g->getQRCodeGoogleUrl('http://aaden001.cs518.cs.odu.edu', $secret).'" />';
+			echo '<img src="'.$g->getQRCodeGoogleUrl('http://192.168.56.152/', $secret).'" />';
+	echo '<form class="form-inline" action="" method="post">
+		<div class="form-group">
+		<label for="inputPassword6">Code Input</label>
+		<input type="text" name="inputTest" id="inputPassword" class="form-control" />
+		<input type="submit" name="SubmitButton"/>
+
+		</div>
+		</form>';
+
+
+		if(isset($_POST['SubmitButton']) && isset($_POST['inputTest']) && isset($_SESSION['userEmail']))
+			$_SESSION['code'] = $_POST['inputTest'];
+				if(isset($_SESSION['code'])){
+					
+
+					$secret = $_SESSION['userEmail'];
+					$check_this_code = $_POST['code'];
+				if ($g->checkCode($secret, $check_this_code)) {
+					echo 'Success!';
+				} else {
+					echo 'Invalid login';
+				}
+		}
+
+*/
    }
 
-if(isset($_POST['SubmitButton']) && isset($_POST['inputTest']) && sset($_SESSION['userEmail']))
-	$_SESSION['code'] = $_POST['inputTest'];
-   if(isset($_SESSION['code'])){
-			$g = new \Google\Authenticator\GoogleAuthenticator();
-			$salt = '7WAO342QFANY6IKBF7L7SWEUU79WL3VMT920VB5NQMW';
-			$secret = $_SESSION['userEmail'].$salt;
-			$check_this_code = $_POST['code'];
-			if ($g->checkCode($secret, $check_this_code)) {
-			echo 'Success!';
-			} else {
-			echo 'Invalid login';
-			}
-   }
+
 
 ?>
