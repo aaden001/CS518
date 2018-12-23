56255bfa3d6336b2a88065674032c86167cd2076
Adeniran Adeniyi <aaden001@odu.edu>
2018-12-23 16:16:31 -0500

    implemented error checking2

--- index.php
+++ index.php
@@ -248,26 +248,9 @@ error_reporting(E_ALL);
         console.log("Image URL: " + profile.getImageUrl());
         console.log("Email: " + profile.getEmail());*/
         
-      }
+      };
 
-      
-       function onLoad() {
-      gapi.load('auth2,signin2', function() {
-        var auth2 = gapi.auth2.init();
-        auth2.then(function() {
-          // Current values
-          var isSignedIn = auth2.isSignedIn.get();
-          var currentUser = auth2.currentUser.get();
-
-          if (!isSignedIn) {
-            // Rendering g-signin2 button.
-            gapi.signin2.render('google-signin-button', {
-              'onsuccess': 'onSignIn'  
-            });
-          }
-        });
-      });
-    }
+    
     </script>
 </body>
 </html>
