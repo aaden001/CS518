5390f78cb05e0029259d4dceab74a7464c38a4cd
Adeniran Adeniyi <aaden001@odu.edu>
2018-12-23 21:17:51 -0500

    new path

--- index.php
+++ index.php
@@ -98,7 +98,7 @@ error_reporting(E_ALL);
   function twitterLogin(){
 
       //require_once 'init.php';
-      require_once 'vendor/autoload.php';
+      require_once 'vendor/jublonet/codebird-php/src/codebird.php';
       require_once 'dbconnect.php';
       require_once 'TwitterAuth.php';
       \Codebird\Codebird::setConsumerKey('PLc68WLvxnkG24zoVuhQKZMzr','XTAuGCw1uMWktSiwMYEgMDGhRFj90Ewalw80XQGymfXgV8mYst');
