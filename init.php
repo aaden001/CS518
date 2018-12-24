901f48bdcabf7ccf2b36da9fd69f9bb79efcf920
Adeniran Adeniyi <aaden001@odu.edu>
2018-12-24 14:27:26 -0500

    displaying full path3

--- init.php
+++ init.php
@@ -2,11 +2,15 @@
 	
 	
 	//require_once 'vendor/jublonet/codebird-php/src/codebird.php';
-	require_once __DIR__ .'/vendor/autoload.php';
-	//require_once __DIR__ .'/vendor/codebird.php';
-	//require_once 'dbconnect.php';
-	require_once __DIR__ .'/TwitterAuth.php';
+	//require_once __DIR__ .'/vendor/autoload.php';
+	if(require_once __DIR__ .'/vendor/autoload.php'){
+		require_once __DIR__ .'/TwitterAuth.php';
 	\Codebird\Codebird::setConsumerKey('PLc68WLvxnkG24zoVuhQKZMzr','XTAuGCw1uMWktSiwMYEgMDGhRFj90Ewalw80XQGymfXgV8mYst');
 	$client = \Codebird\Codebird::getInstance();
+	}else
+	echo 'Not found';
+	//require_once __DIR__ .'/vendor/codebird.php';
+	//require_once 'dbconnect.php';
+	
 	///var_dump($client);
 ?>
