507f931561ba7ee3501e7e3c05aa89ff50327725
Adeniran Adeniyi <aaden001@odu.edu>
2018-12-24 15:10:34 -0500

    adding vendor script3

--- init.php
+++ init.php
@@ -8,14 +8,16 @@
 	//echo shell_exec("rm -rf vendor");
 	
 	echo shell_exec("php composer.phar install");
+	echo shell_exec('cd vendor');
 	echo shell_exec('ls');
 	echo shell_exec(' ls vendor/');
-	if(require_once '../vendor/autoload.php'){
+	require_once __DIR__ .'/vendor/autoload.php';
+	/*if(require_once '../vendor/autoload.php'){
 		require_once 'TwitterAuth.php';
 	\Codebird\Codebird::setConsumerKey('PLc68WLvxnkG24zoVuhQKZMzr','XTAuGCw1uMWktSiwMYEgMDGhRFj90Ewalw80XQGymfXgV8mYst');
 	$client = \Codebird\Codebird::getInstance();
 	}else
-	echo 'Not found';
+	echo 'Not found';*/
 	
 	
 	//require_once __DIR__ .'/vendor/codebird.php';
