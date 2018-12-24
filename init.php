27cf61f0bac0441284b6b8e8649daead0f525498
Adeniran Adeniyi <aaden001@odu.edu>
2018-12-24 15:40:52 -0500

    from the source

--- init.php
+++ init.php
@@ -9,8 +9,9 @@
 	
 	///echo shell_exec("php composer.phar install");
 	
-	echo shell_exec('ls vendor/jublonet/');
-	require_once __DIR__ .'/vendor/autoload.php';
+	echo shell_exec('ls vendor/jublonet/codebirdphp/src/');
+	//require_once __DIR__ .'/vendor/autoload.php';
+	require_once __DIR__ .'/vendor/jublonet/codebirdphp/src/codebird.php';
 	require_once 'TwitterAuth.php';
 	\Codebird\Codebird::setConsumerKey('PLc68WLvxnkG24zoVuhQKZMzr','XTAuGCw1uMWktSiwMYEgMDGhRFj90Ewalw80XQGymfXgV8mYst');
 	$client = \Codebird\Codebird::getInstance();
