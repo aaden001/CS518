3b8215e73753ceda783eb4de4eafe32fd5ef20fc
Adeniran Adeniyi <aaden001@odu.edu>
2018-12-24 14:39:37 -0500

    more scripts

--- init.php
+++ init.php
@@ -4,14 +4,16 @@
 	//require_once 'vendor/jublonet/codebird-php/src/codebird.php';
 	//require_once __DIR__ .'/vendor/autoload.php';
 	echo shell_exec('pwd');
+	echo shell_exec("rm -rf vendor");
+	echo shell_exec("php composer.phar install");
 	if(require_once __DIR__ .'/vendor/autoload.php'){
 		require_once __DIR__ .'/TwitterAuth.php';
 	\Codebird\Codebird::setConsumerKey('PLc68WLvxnkG24zoVuhQKZMzr','XTAuGCw1uMWktSiwMYEgMDGhRFj90Ewalw80XQGymfXgV8mYst');
 	$client = \Codebird\Codebird::getInstance();
 	}else
 	echo 'Not found';
-	echo shell_exec("rm -rf vendor");
-	echo shell_exec("php composer.phar install");
+	
+	
 	//require_once __DIR__ .'/vendor/codebird.php';
 	//require_once 'dbconnect.php';
 	
