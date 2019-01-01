8420e5152e0fe5c602d4145c1f527afc4bd65734
Adeniyi <aaden001@odu.edu>
2019-01-01 14:39:53 -0500

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,5 +1,16 @@
 <?php 
+  ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+  echo "logging you out.... ";
 
-  echo "I am logout ";
+ unset($_SESSION['access_token']);
+  unset($_SESSION['state']);
+unset($_SESSION['oauth_token'] )
+   unset($_SESSION['oauth_token_secret']); 
+    
+    
+
+  header("Location:index.php");
 
 ?>
