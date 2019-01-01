b2f04a6682e893bfeebdfe1c2075b515586015e1
Adeniyi <aaden001@odu.edu>
2019-01-01 14:49:20 -0500

    Update logout.php

--- logout.php
+++ logout.php
@@ -9,7 +9,17 @@ session_destroy();
   unset($_SESSION['state']);
 unset($_SESSION['oauth_token'] );
    unset($_SESSION['oauth_token_secret']); 
-    
+
+
+    if (isset($_SERVER['HTTP_COOKIE'])) {
+    $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
+    foreach($cookies as $cookie) {
+        $parts = explode('=', $cookie);
+        $name = trim($parts[0]);
+        setcookie($name, '', time()-1000);
+        setcookie($name, '', time()-1000, '/');
+    }
+}
     
 
   header("Location:index.php");
